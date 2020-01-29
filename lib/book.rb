ea96567c436f5269f7001fad91a38f0686b9aeb1
jbshipman <jbshipman@icloud.com>
2020-01-28 21:09:52 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,49 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title 
 
+  def initialize(title)
+    @title = title
+  end 
 
+  # # getter for title
+  # def title
+  #   @title
+  # end 
+
+  # # setter for author
+  # def author=(author)
+  #   @author = author
+  # end
+
+  # # getter for author
+  # def author
+  #   @author
+  # end 
+
+  # # getter page_count
+  # def page_count
+  #   @page_count
+  # end 
+
+  # # setter page_count
+  # def page_count=(page_count)
+  #   @page_count = page_count
+  # end
+
+  # # getter genre
+  # def genre
+  #   @genre
+  # end
+
+  # # setter genre
+  # def genre=(genre)
+  #   @genre = genre
+  # end
+
+  # turn_page
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end 
